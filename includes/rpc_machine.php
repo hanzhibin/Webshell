a6a1ed0f6f5e2b746f8b355b300f0a1299dc3b81
JerryTang <jtang@suse.com>
2013-10-12 11:15:22 +0800

    update rpc code

--- includes/rpc_machine.php
+++ includes/rpc_machine.php
@@ -15,7 +15,7 @@ class machine
   public function machine_list()
   {
 
-    $this->db->pre('SELECT unique_id,name,arch.arch,ip,product.product,release.release,
+    $this->db->pre('SELECT machine_id, unique_id,name,arch.arch,ip,product.product,release.release,
                      memsize,disksize,description,machine_status.machine_status,
                      anomaly,serialconsole,powerswitch,consoledevice,consolespeed,
                      default_option,type,powertype,powerslot,
@@ -24,22 +24,37 @@ class machine
                      LEFT JOIN machine_status on machine.machine_status_id=machine_status.machine_status_id
                      LEFT JOIN product on product.product_id=machine.product_id 
                      LEFT JOIN `release` on machine.release_id=release.release_id');
-    return $this->db->exe();
-  }
-  public function machine_get($name)
-  {
-    $this->db->pre('SELECT unique_id,name,arch.arch,ip,product.product,release.release,
-                     memsize,disksize,description,machine_status.machine_status,
-                     anomaly,serialconsole,powerswitch,consoledevice,consolespeed,
-                     default_option,type,powertype,powerslot,
-                     kernel,`usage` FROM machine 
-                     LEFT JOIN arch on machine.arch_id=arch.arch_id 
-                     LEFT JOIN machine_status on machine.machine_status_id=machine_status.machine_status_id
-                     LEFT JOIN product on product.product_id=machine.product_id 
-                     LEFT JOIN `release` on machine.release_id=release.release_id where machine.name=:name');
-    $this->db->bind(':name',$name);
-    return $this->db->exe();
+    //return $this->db->exe();
+    $all_machines = $this->db->exe();
+    $new_all=array();
+    foreach($all_machines as $machine)
+    {
+      $this->db->pre('SELECT user.name FROM user_machine LEFT JOIN user ON user.user_id=user_machine.user_id WHERE machine_id=:machine_id');
+      $this->db->bind(':machine_id',$machine['machine_id']);
+      $resv=$this->db->exe();
+      $resva=array();
+      foreach($resv as $a)
+      {
+        $resva[]=$a['name'];
+      }
+      $resvs=join(',',$resva);
+     
+      $machine['reservation']=$resvs;
+      $new_all[]=$machine;
+    }
+  return $new_all;
     
+
+  }
+  public function machine_get($id)
+  {
+
+    $new_all = array();
+    $all=$this->machine_list();
+    foreach($all as $machine)
+    {
+       if($machine['machine_id']==$id) return $machine; 
+    }
 
   }
   public function machine_search($str)
@@ -54,6 +69,11 @@ class machine
 
   }
 
+  public function machine_free()
+  {
+    
+
+  }
 }
 
 
--- rpc_job.php
+++ rpc_job.php
@@ -2,15 +2,16 @@
     require "globals.php";
     require_once "include/jsonRPCServer.php";
     require_once "include/rpc_pdo.php";
-    require "include/rpc_machine.php";
-    $m = new machine();
+    require "include/rpc_job.php";
+    $m = new job();
 
-    //$a=$m->machine_list();
+    //$a=$m->job_list();
+    $a=$m->job_get('27');
     //$a=$m->machine_get("linux-64r5");
     //$a=$m->machine_search('zbhan');
-    //echo json_encode($a);
+    echo json_encode($a);
     
-    jsonRPCServer::handle($m) or print 'no request';
+    //jsonRPCServer::handle($m) or print 'no request';
 ?>
 
 
